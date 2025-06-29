3db24ff197e2616b05d2cb034f4659c13271862d
Brandon D. <84109908+BrandonDelM@users.noreply.github.com>
2025-06-29 16:07:41 -0700

    Adding parts to a template website

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import './Header.css'
+
+function Header(){
+    return(
+        <div class="Header">
+            <div class="Text">
+                Brandon Del Mundo
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Hero.js
@@ -0,0 +1,11 @@
+import './Hero.css';
+
+function Hero(){
+    return(
+        <div class="Panel">
+            Insert image here
+        </div>
+    )
+}
+
+export default Hero;
