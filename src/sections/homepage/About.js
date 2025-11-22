8ed870c8152300bfd03804f5cc2115effc40ed9c
Brandon D. <84109908+BrandonDelM@users.noreply.github.com>
2025-11-21 21:56:58 -0800

    Putting more links

--- src/sections/homepage/About.js
+++ src/sections/homepage/About.js
@@ -7,9 +7,9 @@ function About(){
         <div className="Hero-Main">
             <div className="Panel">
                 <p className="hero-title">I want to use this website as an outlet for my projects and experimental ideas!</p>
-                <p className="hero-paragraph">Whether it be tampering with game making or a coding language, I'm hopeful that this web page can be used as an outlet for new ideas and creations.</p>
-                <p className="hero-paragraph">By scrolling through this website, you're already viewing my exploration of website development!</p>
-                <p className="hero-paragraph">And along with new projects, I also want to use this website to also host past projects that have been left collecting dust. I find them to be great examples of my growth in coding over the years!</p>
+                <p className="hero-paragraph">As I progress through my journey as a computer science undergraduate, I been really interested in seeking out any opportunity to apply my knowledge.</p>
+                <p className="hero-paragraph">And as someone who is interested in Computer Science's interdisciplinary traits, I love to explore fields such as data science, cognitive science, electrical engineering, etc.</p>
+                <p className="hero-paragraph">So whether it be <a href="https://github.com/BrandonDelM/GDCGJ2025">tampering with game making</a> or <a href="https://github.com/BrandonDelM/Tic-Tac-Two-Final">learning a new coding language</a>, I'm hopeful that this web page can be used as an outlet for new ideas and creations.</p>
             </div>
             <div className="Panel about-panel-wrapper" id="Panel-hover">
                 <img src={snippet} alt="Code snippet" className="snippet"/>
